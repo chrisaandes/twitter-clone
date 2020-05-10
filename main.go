db1387320997271022d55205a22c9f9f802006f6
Christian España <espanasilva33@gmail.com>
2020-05-09 23:45:20 -0500

    chore: add main

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/chrisaandes/twitter-clone/db"
+	"github.com/chrisaandes/twitter-clone/handlers"
+	"github.com/chrisaandes/twitter-clone/handlers/handlers.go"
+)
+
+func main() {
+	if !db.CheckConnection() {
+		log.Fatal("mongo no connect")
+		return
+	}
+
+	handlers.Router()
 }
